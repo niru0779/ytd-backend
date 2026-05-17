12b003c374d2231c0c88cb44b7dc59240f7cb936
niru0779 <narendramygov@gmail.com>
2026-05-17 14:50:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-
-from flask import Flask, request, jsonify
-import yt_dlp
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'YTD Backend Running'
-
 @app.route('/download')
 def download():
 
@@ -20,39 +10,42 @@ def download():
 
     try:
 
-  ydl_opts = {
-    'quiet': True,
-    'format': 'best',
-    'cookiefile': 'cookies.txt',
+        ydl_opts = {
 
-    'extractor_args': {
-        'youtube': {
-            'player_client': ['android']
-        }
-    },
+            'quiet': True,
+            'format': 'best',
 
-    'http_headers': {
-        'User-Agent': 'Mozilla/5.0'
-    }
+            'cookiefile': 'cookies.txt',
 
-}
+            'extractor_args': {
+                'youtube': {
+                    'player_client': ['android']
+                }
+            },
+
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0'
+            }
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
             info = ydl.extract_info(url, download=False)
 
-            formats = info.get('formats', [])
+            download_url = info.get('url')
 
-            download_url = None
+            if not download_url:
 
-            for f in reversed(formats):
+                formats = info.get('formats', [])
 
-                if f.get('url'):
-                    download_url = f['url']
-                    break
+                for f in reversed(formats):
+
+                    if f.get('url'):
+                        download_url = f['url']
+                        break
 
             return jsonify({
+
                 "title": info.get('title'),
                 "thumbnail": info.get('thumbnail'),
                 "download": download_url,
@@ -64,6 +57,3 @@ def download():
         return jsonify({
             "error": str(e)
         })
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
